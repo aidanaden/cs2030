3ec504606f79eb2e7bcd23a672c06879be1b7470
Ryan Aidan <aidanaden@hotmail.com>
2020-10-30 02:50:47 +0800

    completed lab 7 again...... z

--- Labs/Lab-7/Lazy.java
+++ Labs/Lab-7/Lazy.java
@@ -1,11 +1,11 @@
 import java.util.NoSuchElementException;
-import java.util.Objects;
+import java.util.Optional;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 
-public class Lazy<T extends Comparable<?>> {
+public class Lazy<T extends Comparable<T>> {
 
     private final Supplier<T> s;
     private boolean isComputed = false;
@@ -15,15 +15,18 @@ public class Lazy<T extends Comparable<?>> {
         this.s = s;
     }
 
-    static public <T extends Comparable<?>> Lazy<T> of(T value) {
+    static public <T extends Comparable<T>> Lazy<T> of(T value) {
 
         Supplier<T> s = () -> (value);
         return Lazy.of(s);
     } 
 
-    static public <T extends Comparable<?>> Lazy<T> of(Supplier<T> s) {
+    static public <T extends Comparable<T>> Lazy<T> of(Supplier<T> s) {
 
-        if (Objects.isNull(s)) {
+        Optional<Supplier<T>> optS = Optional.ofNullable(s);
+        Optional<Supplier<T>> optE = Optional.empty();
+
+        if (optS == optE) {
             throw new NoSuchElementException("No value present");
         }
 
@@ -32,7 +35,10 @@ public class Lazy<T extends Comparable<?>> {
 
     public boolean isComputed() {
 
-        if (Objects.isNull(this.value)) {
+        Optional<T> optValue = Optional.ofNullable(this.value);
+        Optional<T> optNull = Optional.empty();
+
+        if (optValue == optNull) {
             return false;
         } else {
             return true;
@@ -54,26 +60,35 @@ public class Lazy<T extends Comparable<?>> {
             this.isComputed = true;
             this.value = this.s.get();
         } 
+
+        Optional<T> optValue = Optional.ofNullable(this.value);
+        Optional<T> optNull = Optional.empty();
+
+        if (optValue == optNull) {
+            throw new NullPointerException();
+        } 
         
-        return Objects.requireNonNull(this.value);
+        return this.value;
     }
 
-    public <U extends Comparable<?>> Lazy<U> map(Function<? super T, ? extends U> mapper) {
+    public <U extends Comparable<U>> Lazy<U> map(Function<? super T, ? extends U> mapper) {
         
-        Supplier<U> s = () -> (mapper.apply(get()));
+        Supplier<U> s = () -> mapper.apply(get());
         return Lazy.of(s);
     }
 
-    public <U extends Comparable<?>> Lazy<U> flatMap(Function<? super T, ? extends Lazy<U>> mapper) {
+    public <U extends Comparable<U>> Lazy<U> flatMap(Function<? super T, ? extends Lazy<U>> mapper) {
         
-        Lazy<U> result = mapper.apply(get());
+        Supplier<U> suppResult = () -> mapper.apply(get()).get();
+        Lazy<U> result = Lazy.of(suppResult);
+
         return result;
     }
 
-    public <U extends Comparable<?>, Z extends Comparable<?>> Lazy<U> combine(Lazy<Z> lazyObj, 
+    public <U extends Comparable<U>, Z extends Comparable<Z>> Lazy<U> combine(Lazy<Z> lazyObj, 
                                                                             BiFunction<T, Z, U> func) {
         
-        Supplier<U> s = () -> (func.apply(get(), lazyObj.get()));
+        Supplier<U> s = () -> func.apply(get(), lazyObj.get());
         return Lazy.of(s);
     }
 
@@ -96,21 +111,18 @@ public class Lazy<T extends Comparable<?>> {
 
     public Lazy<Integer> compareTo(Lazy<T> lazyObj) {
 
-        if ((Integer) get() < (Integer) lazyObj.get()) {
+        // Object objLazyObj = (Object) lazyObj.get();
 
-            Supplier<Integer> s = () -> (-1);
-            return Lazy.of(s);
+        if (get().equals(lazyObj.get())) {
 
-        } else if ((Integer) get() > (Integer) lazyObj.get()) {
-
-            Supplier<Integer> s = () -> (1);
-            return Lazy.of(s);
-
-        } else {
-            
-            // else if ((Integer) get() == (Integer) lazyObj.get())
             Supplier<Integer> s = () -> (0);
             return Lazy.of(s);
+            
+        } else {
+
+            Supplier<Integer> value = () -> get().compareTo(lazyObj.get());
+
+            return Lazy.of(value);
         }
     }
 }
--- Labs/Lab-7/LazyList.java
+++ Labs/Lab-7/LazyList.java
@@ -6,7 +6,7 @@ import java.util.function.UnaryOperator;
 import java.util.stream.Stream;
 import java.util.stream.Collectors;
 
-class LazyList<T extends Comparable<?>> {
+class LazyList<T extends Comparable<T>> {
 
     private List<Lazy<T>> list;
     
@@ -14,7 +14,7 @@ class LazyList<T extends Comparable<?>> {
         this.list = list;
     }
 
-    static <T extends Comparable<?>> LazyList<T> generate(int n, T seed, UnaryOperator<T> f) {
+    static <T extends Comparable<T>> LazyList<T> generate(int n, T seed, UnaryOperator<T> f) {
 
         Lazy<T> lazySeed = Lazy.of(seed);
         
